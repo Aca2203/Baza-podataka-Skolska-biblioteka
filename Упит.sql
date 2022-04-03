5b39cf92c5897b36a90a3d5fd3b5eb69797b668c
Александар Стефановић <stefanovicsalex@gmail.com>
2022-04-03 18:48:01 +0200

    Ажурирање документације и креирање базе података
    1. Уклањање атрибута ,,opis'' из табеле ,,Uloga''
    2. Замена табеле ,,Zanr'' табелом ,,Knjizevni_rod''
    3. Замена страног кључа ,,zanr_id'' кључем ,,knjizevni_rod_id'' у табели ,,Knjiga''

---
+++ Упит.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE [Skolska biblioteka];
+USE [Skolska biblioteka];
+
+CREATE TABLE Uloga(
+  id INT PRIMARY KEY,
+  naziv NVARCHAR(50) NOT NULL,
+  opis NVARCHAR(500)
+);
+
+INSERT INTO Uloga (id, naziv, opis) VALUES (),
+(),
+();
